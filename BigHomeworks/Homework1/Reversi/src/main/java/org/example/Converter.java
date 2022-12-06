bebf677bdab9cd9857f3472ab5dfd34b7ad30d93
Dmitry-Rosinskiy <118693256+Dmitry-Rosinskiy@users.noreply.github.com>
2022-12-07 00:17:17 +0300

    Update Converter.java
    Added comments

--- BigHomeworks/Homework1/Reversi/src/main/java/org/example/Converter.java
+++ BigHomeworks/Homework1/Reversi/src/main/java/org/example/Converter.java
@@ -1,14 +1,33 @@
 package org.example;
 
+/**
+ * Класс конвертера.
+ */
 public final class Converter {
+    /**
+     * Преобразует координаты в позицию.
+     * @param row индекс строки
+     * @param col индекс столбца
+     * @return строковое представление позиции
+     */
     public static String convertToPosition(int row, int col) {
         return "" + (char) ('a' + col) + (row + 1);
     }
 
+    /**
+     * Получает из строкового представления позиции координату по строке.
+     * @param pos строковое представление позиции
+     * @return индекс строки
+     */
     public static int convertToRow(String pos) {
         return pos.charAt(1) - '0' - 1;
     }
 
+    /**
+     * Получает из строкового представления позиции координату по столбцу.
+     * @param pos строковое представление позиции
+     * @return индекс столбца
+     */
     public static int convertToCol(String pos) {
         return pos.charAt(0) - 'a';
     }
