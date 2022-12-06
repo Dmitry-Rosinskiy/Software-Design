1597971d75a7e85c214ff56d43fff28ca51346e1
Dmitry-Rosinskiy <118693256+Dmitry-Rosinskiy@users.noreply.github.com>
2022-12-07 00:20:45 +0300

    Update Main.java
    Added comments

--- BigHomeworks/Homework1/Reversi/src/main/java/org/example/Main.java
+++ BigHomeworks/Homework1/Reversi/src/main/java/org/example/Main.java
@@ -1,6 +1,13 @@
 package org.example;
 
+/**
+ * Основной класс программы.
+ */
 public class Main {
+    /**
+     * Точка входа в программу.
+     * @param args входные аргументы
+     */
     public static void main(String[] args) {
         var game = new Reversi();
         game.start();
