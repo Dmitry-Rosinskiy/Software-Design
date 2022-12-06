1a9eae85e182ff5a712baa040327e3c65260be62
Dmitry-Rosinskiy <118693256+Dmitry-Rosinskiy@users.noreply.github.com>
2022-12-07 00:18:11 +0300

    Update Disk.java
    Added comments

--- BigHomeworks/Homework1/Reversi/src/main/java/org/example/Disk.java
+++ BigHomeworks/Homework1/Reversi/src/main/java/org/example/Disk.java
@@ -1,16 +1,33 @@
 package org.example;
 
-public class Disk implements HasSymbol {
+/**
+ * Класс фишки.
+ */
+public final class Disk implements HasSymbol {
+    /**
+     * Цвет фишки.
+     */
     private Color color;
 
+    /**
+     * Возвращает цвет фишки.
+     * @return цвет фишки
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Конструктор фишки по цвету.
+     * @param color цвет
+     */
     Disk(Color color) {
         this.color = color;
     }
 
+    /**
+     * Меняет цвет фишки на противоположный.
+     */
     public void changeColor() {
         if (color == Color.BLACK) {
             color = Color.WHITE;
@@ -19,6 +36,10 @@ public class Disk implements HasSymbol {
         }
     }
 
+    /**
+     * Возвращает символьное представление фишки.
+     * @return симольное представление фишки
+     */
     @Override
     public char getSymbol() {
         if (color == Color.BLACK) {
