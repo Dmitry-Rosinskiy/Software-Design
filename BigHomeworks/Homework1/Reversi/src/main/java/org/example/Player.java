b9b9d250b4f5ba1d00e5e49a3bbc56cbf9771b27
Dmitry-Rosinskiy <118693256+Dmitry-Rosinskiy@users.noreply.github.com>
2022-12-07 00:23:08 +0300

    Update Player.java
    Added comments

--- BigHomeworks/Homework1/Reversi/src/main/java/org/example/Player.java
+++ BigHomeworks/Homework1/Reversi/src/main/java/org/example/Player.java
@@ -3,12 +3,25 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Класс игрока.
+ */
 public final class Player extends Gamer {
 
+    /**
+     * Конструктор игрока по имени и игровому цвету.
+     * @param name имя
+     * @param color цвет
+     */
     public Player(String name, Color color) {
         super(name, color, false);
     }
 
+    /**
+     * Делает ход.
+     * @param moves список возможных ходов
+     * @return выбранный ход
+     */
     @Override
     public String makeMove(ArrayList<String> moves) {
         Scanner in = new Scanner(System.in);
