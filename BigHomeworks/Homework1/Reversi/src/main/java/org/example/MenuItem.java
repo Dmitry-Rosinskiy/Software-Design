c0530424f6157934fd033ab454c474891433e713
Dmitry-Rosinskiy <118693256+Dmitry-Rosinskiy@users.noreply.github.com>
2022-12-07 00:22:50 +0300

    Update MenuItem.java
    Added comments

--- BigHomeworks/Homework1/Reversi/src/main/java/org/example/MenuItem.java
+++ BigHomeworks/Homework1/Reversi/src/main/java/org/example/MenuItem.java
@@ -1,22 +1,49 @@
 package org.example;
 
-public class MenuItem {
+/**
+ * Класс пункта меню.
+ */
+public final class MenuItem {
+    /**
+     * Длина пункта меню.
+     */
     private int length = 50;
+    /**
+     * Верхний текст пункта меню.
+     */
     private String title = "";
+    /**
+     * Нижний текст пункта меню.
+     */
     private String subtitle = "";
 
+    /**
+     * Устанавливает длину пункта меню.
+     * @param length длина пункта меню.
+     */
     public void setLength(int length) {
         this.length = length;
     }
 
+    /**
+     * Устанавливает верхний текст пункта меню.
+     * @param title верхний текст
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Устанавливает нижний текст пункта меню.
+     * @param subtitle нижний текст
+     */
     public void setSubtitle(String subtitle) {
         this.subtitle = subtitle;
     }
 
+    /**
+     * Выводит пункт меню.
+     */
     public void show() {
         for (int i = 0; i < length; ++i) {
             System.out.print("-");
