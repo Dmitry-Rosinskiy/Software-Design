765e858dd93508dc99b0c4d0d74a68cce97c11c2
Dmitry-Rosinskiy <118693256+Dmitry-Rosinskiy@users.noreply.github.com>
2022-12-07 00:20:22 +0300

    Update LeaderBoardItem.java
    Added comments

--- BigHomeworks/Homework1/Reversi/src/main/java/org/example/LeaderBoardItem.java
+++ BigHomeworks/Homework1/Reversi/src/main/java/org/example/LeaderBoardItem.java
@@ -1,32 +1,73 @@
 package org.example;
 
-public class LeaderBoardItem {
+/**
+ * Класс пункта таблицы лучших результатов.
+ */
+public final class LeaderBoardItem {
+    /**
+     * Длина ячейки пункта таблицы для имени.
+     */
     private int nameLength = 100;
+    /**
+     * Длина ячейки пункта таблицы для счёта.
+     */
     private int scoreLength = 10;
+    /**
+     * Пункт таблицы является последним.
+     */
     private boolean isLast = false;
+    /**
+     * Имя в пункте таблицы.
+     */
     private String name = "";
+    /**
+     * Счёт в пункте таблицы.
+     */
     private int score = 0;
 
+    /**
+     * Устанавливает длину ячейки пункта таблицы для имени.
+     * @param length длина ячейки пункта таблицы
+     */
     public void setNameLength(int length) {
         nameLength = length;
     }
 
+    /**
+     * Устанавливает длину ячейки пункта таблицы для счёта.
+     * @param length длина ячейки пункта таблицы
+     */
     public void setScoreLength(int length) {
         scoreLength = length;
     }
 
+    /**
+     * Устанавливает пункт таблицы последним.
+     * @param isLast пункт таблицы является последним
+     */
     public void setIsLast(boolean isLast) {
         this.isLast = isLast;
     }
 
+    /**
+     * Устанавливает имя в пункте таблицы.
+     * @param name имя
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Устанавливает счёт в пункте таблицы.
+     * @param score счёт
+     */
     public void setScore(int score) {
         this.score = score;
     }
 
+    /**
+     * Выводит пункт таблицы.
+     */
     public void show() {
         for (int i = 0; i < nameLength + scoreLength - 1; ++i) {
             System.out.print("-");
