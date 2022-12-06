bf358a3e1b0e39e3ba6fd9895e9afc5f1054e712
Dmitry-Rosinskiy <118693256+Dmitry-Rosinskiy@users.noreply.github.com>
2022-12-07 00:20:00 +0300

    Update LeaderBoard.java
    Added comments

--- BigHomeworks/Homework1/Reversi/src/main/java/org/example/LeaderBoard.java
+++ BigHomeworks/Homework1/Reversi/src/main/java/org/example/LeaderBoard.java
@@ -2,28 +2,63 @@ package org.example;
 
 import java.util.ArrayList;
 
-public class LeaderBoard {
+/**
+ * Класс таблицы лучших рузультатов.
+ */
+public final class LeaderBoard {
+    /**
+     * Длина ячейки таблицы для имени.
+     */
     private int nameLength = 60;
+    /**
+     * Длина ячейки таблицы для счёта.
+     */
     private int scoreLength = 6;
-    private ArrayList<LeaderBoardItem> LEADER_BOARD_ITEM_LIST = new ArrayList<>();
+    /**
+     * Список пунктов таблицы.
+     */
+    private final ArrayList<LeaderBoardItem> LEADER_BOARD_ITEM_LIST = new ArrayList<>();
+    /**
+     * Заголовок таблицы.
+     */
     private String title = "";
 
+    /**
+     * Устанавливает длину ячейки таблицы для имени.
+     * @param length длина ячейки
+     */
     public void setNameLength(int length) {
         nameLength = length;
     }
 
+    /**
+     * Устанавливает длину ячейки таблицы для счёта.
+     * @param length длина ячейки
+     */
     public void setScoreLength(int length) {
         this.scoreLength = length;
     }
 
+    /**
+     * Устанавливает заголовок таблицы.
+     * @param title заголовок таблицы
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Возвращение пункта таблицы по индексу
+     * @param index индекс пункта
+     * @return пункт таблицы
+     */
     public LeaderBoardItem getLeaderBoardItem(int index) {
         return LEADER_BOARD_ITEM_LIST.get(index);
     }
 
+    /**
+     * Выводит таблицу.
+     */
     public void show() {
         System.out.println(title);
         for (LeaderBoardItem item : LEADER_BOARD_ITEM_LIST) {
@@ -31,6 +66,11 @@ public class LeaderBoard {
         }
     }
 
+    /**
+     * Добавляет пункт таблицы.
+     * @param name имя
+     * @param score счёт
+     */
     public void addLeaderBoardItem(String name, int score) {
         var leaderBoardItem = new LeaderBoardItem();
         leaderBoardItem.setNameLength(nameLength);
